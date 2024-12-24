f0d6f8adafba9e5d5f7210d084477540dbf6e23a
max <161075989+maxc0des@users.noreply.github.com>
2024-12-24 11:47:04 +0100

    outsourced the functions to get data

---
+++ get_data.py
@@ -0,0 +1,17 @@
+#this script gets the data from all the sensors
+
+def take_photo():
+    print("taking a photo")
+    #take the photo
+    #save it
+    #return the path
+
+def get_gyro():
+    print("getting the gyro data")
+    #get the data
+    #return the value
+
+def get_tof():
+    print("getting the tof data")
+    #get the data
+    #return the values (3!!)
